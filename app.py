bcea954df52d7a6119fe4ea1190c9783dcd558f8
Peacock-wealth <peacockwealthmanagement@gmail.com>
2025-11-12 13:04:52 +0530

    Fixed CORS origin for frontend

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from urllib.parse import quote_plus
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
-    "https://peacockfrontends.onrender.com",
+    "https://peacockfrontend.onrender.com",
     "http://localhost:5500"
 ]}}) # Frontend (Live Server) can talk to backend
 
